138db6cbead3bbf736bcaa040a2ba6674054140e
Thangaraj-dev <thangaraj.kubendran@marlabs.com>
2026-06-24 19:54:11 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,17 @@
+// Program.cs
+using System;
+
+Console.WriteLine("Welcome to the .NET Sample Application!");
+Console.Write("Please enter your name: ")
+
+string? userName = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(userName))
+{
+    string upperName = userName.ToUpper();
+    Console.WriteLine($"Hello, {upperName}! Welcome aboard.");
+
+else
+{
+    Console.WriteLine("Hello, anonymous user!");
+}
