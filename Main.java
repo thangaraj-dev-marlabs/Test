224a242e76cd49e4aef862c01278a88f96c8d087
thangaraj-java-dev <thangaraj.kubendran@marlabs.com>
2026-06-24 10:30:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
-    static void main() {
-        int Number =30;
+    public static void main(String[] args)
+        int number = 30;
         System.out.println("Hello java");
     }
 }
