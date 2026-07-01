5d65f91c3e6818a1baba48bbff200661bb6186a7
Thangaraj-dev <thangaraj.kubendran@marlabs.com>
2026-07-01 11:49:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
         float value1= 10.2;
         float value2 = 11.3;
         float sum = value1+value2;
-        System.out.println("Sum of two value :"+sum);
+        System.out.println(sum);
     }
 }
