5318c3f51d15f212a8024e31d05bc2a161b4e30c
Thangaraj-dev <thangaraj.kubendran@marlabs.com>
2026-07-09 21:51:52 +0530

    Update WebhookController.java

--- WebhookController.java
+++ WebhookController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/webhook")
 public class WebhookController {
 
     private final WebhookVerificationService verificationService;
